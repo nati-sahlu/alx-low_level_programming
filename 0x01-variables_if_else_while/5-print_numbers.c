6ea6d35d84235abd6c4685f07608e480dd3d8eba
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 07:12:03 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main - Prog Entry point
+ *
+ *  Return: Always 0 (success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
