c4504868ed586531ee57fac3eeee040e5e92e86b
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 07:53:23 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *  main - Prog Entry point
+ *
+ *  Retrun: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
