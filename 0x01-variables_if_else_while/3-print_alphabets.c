6c1f5ed0c98d9af16ece1aa575f4292e4b0496c3
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 06:13:21 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,23 +6,19 @@
 */
 int main(void)
 {
-    char c = 'a';
-    char d = 'A';
-
-    /* Print lowercase alphabet */
-    while (c <= 'z') {
-        putchar(c);
-        c++;
-    }
-
-    /* Print uppercase alphabet */
-    while (d <= 'Z') {
-        putchar(d);
-        d++;
-    }
-
-    /* Print newline */
-    putchar('\n');
-
-    return (0);
+	char c = 'a';
+	char d = 'A';
+	/* Print lowercase alphabet */
+	while (c <= 'z') {
+		putchar(c);
+		c++;
+	}
+	/* Print uppercase alphabet */
+	while (d <= 'Z') {
+		putchar(d);
+		d++;
+	}
+	/* Print newline */
+	putchar('\n');
+	return (0);
 }
