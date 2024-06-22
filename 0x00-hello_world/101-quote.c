fbf2862948e636d54601756cf08c418630c90792
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 16:58:00 -0700

    gydsert

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - A C program that prints a message
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+    fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",stdout);
     return (1);
 }
