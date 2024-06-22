d89c719997ca0bf1efe2ac93efb235484228a2dd
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 10:11:52 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0;i < 100; j++)
+	for (i = 0 ; i < 100; j++)
 	{
 		for (j = 0; j  < 100; j++)
 		{
 			if (i < j)
 			{
-				putchar((i/10) + 48);
+				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
