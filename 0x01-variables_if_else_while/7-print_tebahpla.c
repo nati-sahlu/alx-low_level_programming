2a3f8e959cd19bb82aa97f25063984d69b123776
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 07:35:05 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - Prog Entry point
+ *
+ *  Return: Always 0 (success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
