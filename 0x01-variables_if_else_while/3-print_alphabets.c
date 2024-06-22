1600a914428ae3f8334dabe4cdcfcdd2b34c7289
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 05:06:23 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  main - Entry point
+ *	main - Entry point
  *
- *  Return: Always 0 (success)
+ *	Return: Always 0 (success)
 */
 /* betty style doc for function main goes there */
 int main(void)
