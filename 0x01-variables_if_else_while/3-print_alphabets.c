b204bc4cbdaf34213981106602f53c39fd0681dd
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 06:05:50 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
-/**
- *  main -Entry point
- *
- *  Return: Always 0 (success)
-*/
-/* betty style doc for function main goes there */
+
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     char c = 'a';
+    char d = 'A';
 
-    // Print lowercase alphabet
+    /* Print lowercase alphabet */
     while (c <= 'z') {
         putchar(c);
         c++;
     }
 
-    c = 'A';
-
-    // Print uppercase alphabet
-    while (c <= 'Z') {
-        putchar(c);
-        c++;
+    /* Print uppercase alphabet */
+    while (d <= 'Z') {
+        putchar(d);
+        d++;
     }
 
-    // Print newline
+    /* Print newline */
     putchar('\n');
 
     return (0);
