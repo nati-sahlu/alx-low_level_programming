7dad53d8a9345b4c2acf64a217d5306e68fab5b0
nati-sahlu <natansahlu5@gmail.com>
2024-06-22 17:14:18 -0700

    vgsdg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
  * main - A C program that prints a message
  *
  * Return: 1 (Success)
- */
+*/
 int main(void)
 {
-    fputs("and that piece of art is useful\" - Dora Korpar, "
-          "2015-10-19\n", stdout);
-    return 1;
+	fputs("and that piece of art is useful\" - Dora Korpar, "
+			"2015-10-19\n", stdout);
+	return (1);
 }
