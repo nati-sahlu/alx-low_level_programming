c74b66a1aaca304451f19faf3f6ffea78468d8f6
nati-sahlu <natansahlu5@gmail.com>
2024-07-04 06:01:19 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	int j;
+	int t;
 
-	for (i = 0; i < n--; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = t;
 	}
 }
