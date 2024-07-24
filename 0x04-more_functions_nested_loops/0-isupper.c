a24bd2a5e820f1232a123dae13902ad628be9d2d
nati-sahlu <natansahlu5@gmail.com>
2024-07-24 02:18:14 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * int  _isupper checks for uppercase charachter
+ * @c: variable test
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+}
