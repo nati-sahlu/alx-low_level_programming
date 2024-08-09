fee943e7725430646fff011c1d6caff5c43b509f
nati-sahlu <natansahlu5@gmail.com>
2024-08-09 12:54:30 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /*
  * main - Entry point
+ * 
+ * Description - main is the entry point
  *
  * Return: Always 0
  */
 
-int main(void) 
+int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
