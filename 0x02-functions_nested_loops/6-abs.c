eaea440ff09d60d0394e517e698d4dc7cd5b7435
nati-sahlu <natansahlu5@gmail.com>
2024-08-11 02:22:41 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/* _abs -  computes the absolute value of an integer.
+ *
+ * Description - This function takes an integer and change it to 
+ * unsigned integer
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int n;
+	int c;
+	
+	c = n % 10;
+       if (c >= 0)
+       {
+		_putchar(n);
+       }
+       else
+       {
+	       n *= -1;
+		       _putchar(n);
+       }
+}
