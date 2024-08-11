3edb86407ab96a98be5170ae4ca897f6afe22855
nati-sahlu <natansahlu5@gmail.com>
2024-08-11 02:04:47 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -23,12 +23,15 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if(c = 0)
+	else if (c = 0)
 	{
-		_putchar('0')
+		_putchar('0');
 	return (0);
 	}
-	else
+	else 
+	{
+		_putchar('-');
 	return (-1);
+	}
 }
 
