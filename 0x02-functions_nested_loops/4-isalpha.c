36259900b2a35ddd7d8c379447ca52fc6c9e4b71
nati-sahlu <natansahlu5@gmail.com>
2024-08-11 01:29:16 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /*
- * _isalpha - checks for uppercase character.
+ * _isalpha - checks for alphabet character.
  *@c - integer
  *
  * Description - this functions take an int and checks if it is a uppercase
- * if true returns 1 otherwise returns 0.
+ * or lowercase it returns 1 otherwise returns 0.
  *
- * Return: Returns 1 if c is lowercase
+ * Return: Returns 1 if c is alphabet character
  * Returns 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 122 && !(90 < c > 97 )
+	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
+	if else(c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
 	return (0);
-
+	}
 }
