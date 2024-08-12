2a39272f6aa78a0cc608b2f0904fd074273ea219
nati-sahlu <natansahlu5@gmail.com>
2024-08-12 05:08:55 -0700

    final

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,6 +6,20 @@
  *
  */
 
+void print_int(int n)
+{
+    if (n < 0)
+    {
+        putchar('-');
+        n = -n;
+    }
+    if (n / 10 != 0)
+    {
+        print_int(n / 10);
+    }
+putchar(n % 10 + '0');
+}
+
 int main(void)
 {
 	int n = 0;
