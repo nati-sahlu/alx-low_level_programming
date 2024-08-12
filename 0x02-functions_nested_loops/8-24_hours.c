1332e40f27ee6e2f3717dc29976e848da72ea2d3
nati-sahlu <natansahlu5@gmail.com>
2024-08-12 01:10:30 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,24 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
+	int k;
+	int f;
 
-	for (i = 0 ; i < 24; i++)
+	for (i = 0 ; i < 3; i++)
 	{
-		for (j = 0; j < 60; j++)
+		for (j = 0; j < 10; j++)
 		{
-			 _putchar(i + '0');
-			_putchar(':');
-			 _putchar(j + '0');
-			  _putchar('\n');
+			for (k = 0; k < 10; k++)
+			{
+				for (f = 0; f < 10; f++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(f + '0');
+				}
+			}
 		}
 	}
 }
