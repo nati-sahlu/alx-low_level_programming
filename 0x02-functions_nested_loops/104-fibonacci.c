3aca65ed6e12658bee654e06eeb80a45f4b4576a
nati-sahlu <natansahlu5@gmail.com>
2024-08-12 03:20:53 -0700

    final

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ *
+ *
+ *
+ */
+
+void main(void)
+{
+	int n = 1;
+	int m;
+	
+	for ( m = 0; m < 100; m + n)
+	{
+		m = n + m;
+		n = m;
+		if (m > 9)
+		{
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+		}
+		else 
+		{
+			 _putchar(m + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+}
