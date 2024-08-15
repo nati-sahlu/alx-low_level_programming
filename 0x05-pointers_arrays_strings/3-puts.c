dbaf7e89a5ebc873b75ccb701ccd1439095213ac
nati-sahlu <natansahlu5@gmail.com>
2024-08-15 02:23:27 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ *
+ *
+ */
+
+void _puts(char *str)
+{
+	char n[5] = {'s', 't', 'r', 'i', 'n', 'g', '/0'}
+
+		for (i = 0; i < 5; i++)
+		{
+			_putchar(i);
+
+		}
+	_putchar('\n');
+}
